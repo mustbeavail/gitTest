b175ac6afe5fddacb58361dead03802baff3e514
kwan_hyung_lee <grick21@gmail.com>
2025-05-16 15:05:37 +0900

    컨트롤러 생성

---
+++ src/main/java/kr/co/himedia/controller/MainController.java
@@ -0,0 +1,15 @@
+package kr.co.himedia.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@GetMapping(value="/")
+	public String main() {
+		
+		return "{\"msg\":\"OK\"}";
+	}
+
+}
