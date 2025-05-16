55cadc1c5ff19ded35a6204c635ced11697d4bcd
kwan_hyung_lee <grick21@gmail.com>
2025-05-16 16:08:55 +0900

    삭제기능 추가

--- src/main/java/kr/co/himedia/controller/MainController.java
+++ src/main/java/kr/co/himedia/controller/MainController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,4 +40,8 @@ public class MainController {
 		return null;
 	}
 
+	@DeleteMapping(value="/del")
+	public ResponseEntity<?> del(){
+		return null;
+	}
 }
